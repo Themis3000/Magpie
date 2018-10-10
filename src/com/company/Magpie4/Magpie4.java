3f544974537dd23f7a141e5bb256efd91beb2e45
gnush2000 <gnush2000@gmail.com>
2018-10-10 12:54:00 -0500

    1

--- src/com/company/Magpie4/Magpie4.java
+++ src/com/company/Magpie4/Magpie4.java
@@ -93,7 +93,7 @@ public class Magpie4
 		}
 		int psn = findKeyword (statement, "I want to", 0);
 		String restOfStatement = statement.substring(psn + 9).trim();
-		return "What would it mean to " + restOfStatement + "?";
+		return "Would you really be happy if you had " + restOfStatement + "?";
 	}
 
 	
@@ -118,9 +118,11 @@ public class Magpie4
 		
 		int psnOfYou = findKeyword (statement, "you", 0);
 		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
-		
+
+
+
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-		return "What makes you think that I " + restOfStatement + " you?";
+		return "Why do you " + restOfStatement + " me?";
 	}
 	
 	
